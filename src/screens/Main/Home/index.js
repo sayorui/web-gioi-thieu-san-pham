b98f6b3c540e642ba14b8278593dfd22cc84bbb6
sayorui <nightmare01413@gmail.com>
2020-05-10 16:29:56 +0700

    update Home sideBar

--- src/screens/Main/Home/index.js
+++ src/screens/Main/Home/index.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import { Carousel } from 'react-bootstrap'
 
 const fakeData = [
-    { ID: 1, Title: '', Image: '../../assets/images/big/img6.jpg', Content: ` <h4 class="card-title">Card title</h4><p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>` },
-    { ID: 2, Title: '', Image: '../../assets/images/big/img3.jpg', Content: ` <h4 class="card-title">Card title</h4><p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>` },
-    { ID: 3, Title: '', Image: '../../assets/images/big/img4.jpg', Content: ` <h4 class="card-title">Card title</h4><p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>` },
-    { ID: 4, Title: '', Image: '../../assets/images/big/img6.jpg', Content: ` <h4 class="card-title">Card title</h4><p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>` },
-    { ID: 5, Title: '', Image: '../../assets/images/big/img3.jpg', Content: ` <h4 class="card-title">Card title</h4><p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>` },
+    { ID: 1, Title: '', Image: '../../assets/images/big/img6.jpg', Content: ` <h4 className="card-title">Card title</h4><p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>` },
+    { ID: 2, Title: '', Image: '../../assets/images/big/img3.jpg', Content: ` <h4 className="card-title">Card title</h4><p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>` },
+    { ID: 3, Title: '', Image: '../../assets/images/big/img4.jpg', Content: ` <h4 className="card-title">Card title</h4><p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>` },
+    { ID: 4, Title: '', Image: '../../assets/images/big/img6.jpg', Content: ` <h4 className="card-title">Card title</h4><p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>` },
+    { ID: 5, Title: '', Image: '../../assets/images/big/img3.jpg', Content: ` <h4 className="card-title">Card title</h4><p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>` },
 ]
 
 export default class Home extends Component {
@@ -34,51 +34,30 @@ export default class Home extends Component {
                 <section id="Carousel">
                     <div className="row">
                         <div className="col-3"></div>
-                        <div className="col-6">
+                        {/* <div className="col-6"> */}
                             <div className="card">
                                 <div className="card-body">
                                     <Carousel>
-                                        <Carousel.Item>
-                                            <img
-                                                className="d-block"
-                                                src="../../assets/images/big/img6.jpg"
-                                                alt="First slide"
-                                                style={{ maxHeight: '300px' }}
-                                            />
-                                            <Carousel.Caption>
-                                                <h3>First slide label</h3>
-                                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                                            </Carousel.Caption>
-                                        </Carousel.Item>
-                                        <Carousel.Item>
-                                            <img
-                                                className="d-block"
-                                                src="../../assets/images/big/img3.jpg"
-                                                alt="Third slide"
-                                                style={{ maxHeight: '300px' }}
-                                            />
-
-                                            <Carousel.Caption>
-                                                <h3>Second slide label</h3>
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                            </Carousel.Caption>
-                                        </Carousel.Item>
-                                        <Carousel.Item>
-                                            <img
-                                                className="d-block"
-                                                src="../../assets/images/big/img4.jpg"
-                                                alt="Third slide"
-                                                style={{ maxHeight: '300px' }}
-                                            />
-
-                                            <Carousel.Caption>
-                                                <h3>Third slide label</h3>
-                                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                                            </Carousel.Caption>
-                                        </Carousel.Item>
+                                        {
+                                            listTimHieuSanPham.map((item, index) => {
+                                                return (
+                                                    <Carousel.Item key={index.toString()}>
+                                                        <img
+                                                            className="d-block"
+                                                            src={item.Image}
+                                                            alt={`${index} slide`}
+                                                            style={{ maxHeight: '300px' }}
+                                                        />
+                                                        <Carousel.Caption>
+                                                            <div dangerouslySetInnerHTML={{ __html: item.Content }}></div>
+                                                        </Carousel.Caption>
+                                                    </Carousel.Item>
+                                                )
+                                            })
+                                        }
                                     </Carousel>
                                 </div>
-                            </div>
+                            {/* </div> */}
                         </div>
                     </div>
                 </section>
@@ -87,10 +66,10 @@ export default class Home extends Component {
                     <div className="row">
                         {/* Đoạn render sử dụng mảng, lấy dữ liệu từ state listTimHieuSanPham */}
                         {
-                            listTimHieuSanPham.map((item) => {
+                            listTimHieuSanPham.map((item, index) => {
                                 console.log(item)
                                 return (
-                                    <div className="col-6">
+                                    <div className="col-6" key={index.toString()}>
                                         <div className="card">
                                             <img
                                                 className="d-block"
@@ -113,10 +92,10 @@ export default class Home extends Component {
                     <div className="row">
                         {/* Đoạn render sử dụng mảng, lấy dữ liệu từ state listBaiVietMoi */}
                         {
-                            listBaiVietMoi.map((item) => {
+                            listBaiVietMoi.map((item, index) => {
                                 console.log(item)
                                 return (
-                                    <div className="col-6">
+                                    <div className="col-6" key={index.toString()}>
                                         <div className="card">
                                             <img
                                                 className="d-block"
