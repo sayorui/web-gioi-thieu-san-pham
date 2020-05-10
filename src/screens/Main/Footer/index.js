d9e20ee15467dfc91d4b836ab4dbbac716947a29
sayorui <nightmare01413@gmail.com>
2020-05-10 16:30:01 +0700

    update footer

--- src/screens/Main/Footer/index.js
+++ src/screens/Main/Footer/index.js
@@ -4,11 +4,11 @@ import { Carousel } from 'react-bootstrap'
 export default class Footer extends Component {
     render() {
         return (
-            <div style={{ marginLeft: '250px' }}>
+            <div style={{ marginLeft: '260px' }}>
                 <div className="row">
                     <div className="col-3">
-                        <h3 class="widget-title">NPP IZUMIO JAPAN</h3>
-                        <div class="textwidget custom-html-widget">NPP IZUMIO JAPAN <br />
+                        <h3 className="widget-title">NPP IZUMIO JAPAN</h3>
+                        <div className="textwidget custom-html-widget">NPP IZUMIO JAPAN <br />
                             – Xuất xứ sản phẩm :  Nhật Bản<br />
                                 + Sản xuất tại: Shefco Co., Ltd. Kanuma Factory.<br />
                                 + Địa chỉ: 990-57 Higashiyama, Fukahodo, Kanuma – shi, Tochigi, Japan<br />
@@ -16,8 +16,16 @@ export default class Footer extends Component {
                                 0968-01-8166</div>
                     </div>
                     <div className="col-3">
-                        <h3 class="widget-title">NPP IZUMIO JAPAN</h3>
-                        <div class="textwidget custom-html-widget">NPP IZUMIO JAPAN <br />
+                        <h3 className="widget-title">CAM KẾT CHẤT LƯỢNG</h3>
+                        <div className="textwidget custom-html-widget">– Sản phẩm chất lượng, nhập khẩu chính hãng 100% từ Nhật Bản<br />
+                            – Giao hàng miễn phí toàn quốc<br />
+                            – SẢN PHẨM ĐẠT HIỆU QUẢ RÕ RỆT SAU 1 LIỆU TRÌNH SỬ DỤNG<br />
+                            ( Được kiểm chứng bởi hàng trăm người đã và đang dùng sản phẩm : <a href="http://izumiojapan.net/cau-chuyen-khach-hang-izumio-super-lutein-mirto-review/">xem tại đây</a> )
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <h3 className="widget-title">NPP IZUMIO JAPAN</h3>
+                        <div className="textwidget custom-html-widget">NPP IZUMIO JAPAN <br />
                             – Xuất xứ sản phẩm :  Nhật Bản<br />
                                 + Sản xuất tại: Shefco Co., Ltd. Kanuma Factory.<br />
                                 + Địa chỉ: 990-57 Higashiyama, Fukahodo, Kanuma – shi, Tochigi, Japan<br />
@@ -25,29 +33,27 @@ export default class Footer extends Component {
                                 0968-01-8166</div>
                     </div>
                     <div className="col-3">
-                        <h3 class="widget-title">NPP IZUMIO JAPAN</h3>
-                        <div class="textwidget custom-html-widget">NPP IZUMIO JAPAN <br />
-                            – Xuất xứ sản phẩm :  Nhật Bản<br />
-                                + Sản xuất tại: Shefco Co., Ltd. Kanuma Factory.<br />
-                                + Địa chỉ: 990-57 Higashiyama, Fukahodo, Kanuma – shi, Tochigi, Japan<br />
-                                # Hỗ trợ hướng dẫn mua hàng :<br />
-                                0968-01-8166</div>
-                    </div>
-                    <div className="col-3">
-                        <h3 class="widget-title">NPP IZUMIO JAPAN</h3>
-                        <div class="textwidget custom-html-widget">NPP IZUMIO JAPAN <br />
-                            – Xuất xứ sản phẩm :  Nhật Bản<br />
-                                + Sản xuất tại: Shefco Co., Ltd. Kanuma Factory.<br />
-                                + Địa chỉ: 990-57 Higashiyama, Fukahodo, Kanuma – shi, Tochigi, Japan<br />
-                                # Hỗ trợ hướng dẫn mua hàng :<br />
-                                0968-01-8166</div>
+                        <h3 className="widget-title">CHI NHÁNH TRÊN TOÀN CẦU</h3>
+                        <div className="textwidget custom-html-widget">
+                            <a rel="nofollow noopener noreferrer" href="http://www.naturally-plus.com/jp/ja/" target="_blank">  NHẬT BẢN </a><br />
+                            <a rel="nofollow noopener noreferrer" href="http://www.npusainc.com/us/en/" target="_blank">  MỸ </a><br />
+                            <a rel="nofollow noopener noreferrer" href="http://www.naturally-plus.com/zh/tw/" target="_blank">  ĐÀI LOAN </a><br />
+                            <a rel="nofollow noopener noreferrer" href="http://www.naturally-plus.com/sg/en/" target="_blank"> SINGAPORE</a><br />
+                            <a rel="nofollow noopener noreferrer" href="http://www.naturally-plus.com/ko/kr/" target="_blank"> HÀN QUỐC </a><br />
+                            <a rel="nofollow noopener noreferrer" href="http://www.naturally-plus.com/zh/hk/" target="_blank"> HONG KONG</a><br />
+                            <a rel="nofollow noopener noreferrer" href="http://www.naturally-plus.com/my/en/" target="_blank"> MALAYSIA</a><br />
+                            <a rel="nofollow noopener noreferrer" href="http://www.naturally-plus.com/th/th/" target="_blank"> THÁI LAN</a><br />
+                            <a rel="nofollow noopener noreferrer" href="http://www.naturally-plus.com/id/id/" target="_blank"> INDONESIA</a><br />
+                            <a rel="nofollow noopener noreferrer" href="http://www.naturally-plus.com/ph/en/products/izumio/overview/" target="_blank"> PHILIPIN</a><br />
+                            <a rel="nofollow noopener noreferrer" href="http://www.naturally-plus.com/bn/en/" target="_blank"> BRUNEI</a><br/>
+                        </div>
                     </div>
                 </div>
-                <div class="copyrights">
-                    <div class="container">
+                <div className="copyrights">
+                    <div className="container">
                         <div id="copyright-note">
                             <span><a href="https://izumiojapan.net/" title="Nhập Khẩu Chính Hãng Nội Địa Nhật Bản" rel="nofollow">IZUMIO JAPAN</a> Copyright © 2020.</span>
-                            <div class="right"></div>
+                            <div className="right"></div>
                         </div>
                     </div>
                 </div>
