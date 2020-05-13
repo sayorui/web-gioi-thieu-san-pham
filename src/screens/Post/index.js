13c3bb747dba461948d4717988a732721977b268
sayorui <nightmare01413@gmail.com>
2020-05-13 23:34:22 +0700

    Update trang thêm mới

--- src/screens/Post/index.js
+++ src/screens/Post/index.js
@@ -25,19 +25,51 @@ export default class Post extends Component {
         const { data } = this.state;
 
         return (
-            <div>
-                <div>
-                    <CKEditor
-                        data={data}
-                        config={{
-                            // toolbar: [['Bold']]
-                            // extraPlugins: 'easyimage'
-                        }}
-                        onChange={(evt) => { console.log(evt.editor.getData()); this.setState({ data: evt.editor.getData() }) }}
-                    />
+            <>
+                <div id="main-wrapper" data-theme="light" data-layout="vertical"
+                    data-navbarbg="skin6" data-sidebartype="full" data-sidebar-position="fixed"
+                    data-header-position="fixed" data-boxed-layout="full" className=""
+                >
+                    <div className="page-wrapper" style={{ display: 'block', marginLeft: 0 }}>
+                        <div className="container-fluid">
+                            <div className="card">
+                                <div className="card-header">
+                                    <h4>
+                                        Nhập nội dung của bài viết
+                                    </h4>
+                                </div>
+
+                                <div className="card-body">
+                                    <h4 class="card-title">Tiêu đề bài viết</h4>
+                                    <div className="form-group" style={{ display: 'flex' }}>
+                                        <input type="text" class="form-control" />
+                                        <button type="submit" class="btn btn-success" style={{ marginLeft: 20 }}> <i class="fa fa-check"></i> Save</button>
+                                    </div>
+                                    <CKEditor
+                                        data={data}
+                                        config={{
+                                            // toolbar: [['Bold']]
+                                            // extraPlugins: 'easyimage'
+                                        }}
+                                        onChange={(evt) => { console.log(evt.editor.getData()); this.setState({ data: evt.editor.getData() }) }}
+                                    />
+                                </div >
+
+                            </div >
+                            <div className="card">
+                                <div className="card-header">
+                                    <h4>
+                                        Nội dung hiển thị trên trang chi tiết
+                                    </h4>
+                                </div>
+                                <div className="card-body">
+                                    <div dangerouslySetInnerHTML={{ __html: data }}></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div dangerouslySetInnerHTML={{ __html: data }}></div>
-            </div>
+            </>
         )
     }
 }
